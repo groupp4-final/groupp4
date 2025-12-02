aad54fe72c7c5958f46737136ec164a74866b924
Emerson1227 <shg32@txstate.edu>
2025-12-01 19:02:45 -0600

    New Poloygons

--- polygon.js
+++ polygon.js
@@ -1,168 +1,167 @@
 
-function box(minLon, minLat, maxLon, maxLat) {
-  return [
-    [minLon, minLat],
-    [minLon, maxLat],
-    [maxLon, maxLat],
-    [maxLon, minLat],
-    [minLon, minLat] 
-  ];
-}
 
-export const sites = {
-  "GuadalupeMountainsNP": {
-    "type": "Feature",
-    "properties": { "name": "Guadalupe Mountains National Park" },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [
-        box(-105.00, 31.70, -104.60, 32.00)
-      ]
-    }
+export const npsPolygons = [
+
+  // 1. Guadalupe Mountains National Park
+  {
+    name: "Guadalupe Mountains National Park",
+    coords: [
+      [-104.92, 31.92],
+      [-104.85, 31.85],
+      [-104.78, 31.88],
+      [-104.77, 31.97],
+      [-104.85, 32.00],
+      [-104.92, 31.96]
+    ]
   },
 
-  "BigBendNP": {
-    "type": "Feature",
-    "properties": { "name": "Big Bend National Park" },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [
-        box(-104.00, 28.90, -102.90, 29.80)
-      ]
-    }
+  // 2. Big Bend National Park
+  {
+    name: "Big Bend National Park",
+    coords: [
+      [-103.60, 29.40],
+      [-103.20, 29.30],
+      [-102.95, 29.15],
+      [-102.90, 28.95],
+      [-103.40, 29.05],
+      [-103.55, 29.20]
+    ]
   },
 
-  "ChamizalNM": {
-    "type": "Feature",
-    "properties": { "name": "Chamizal National Memorial" },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [
-        box(-106.52, 31.75, -106.46, 31.81)
-      ]
-    }
+  // 3. Chamizal National Memorial
+  {
+    name: "Chamizal National Memorial",
+    coords: [
+      [-106.50, 31.78],
+      [-106.48, 31.78],
+      [-106.48, 31.77],
+      [-106.50, 31.77]
+    ]
   },
 
-  "FortDavisNHS": {
-    "type": "Feature",
-    "properties": { "name": "Fort Davis National Historic Site" },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [
-        box(-103.93, 30.56, -103.86, 30.63)
-      ]
-    }
+  // 4. Fort Davis National Historic Site
+  {
+    name: "Fort Davis National Historic Site",
+    coords: [
+      [-103.92, 30.59],
+      [-103.90, 30.59],
+      [-103.90, 30.57],
+      [-103.92, 30.57]
+    ]
   },
 
-  "RioGrandeWSR": {
-    "type": "Feature",
-    "properties": { "name": "Rio Grande Wild & Scenic River" },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [
-        box(-104.00, 28.90, -102.90, 29.80)
-      ]
-    }
+  // 5. Rio Grande Wild & Scenic River
+  {
+    name: "Rio Grande Wild & Scenic River",
+    coords: [
+      [-103.60, 29.30],
+      [-103.10, 29.20],
+      [-102.90, 28.95],
+      [-103.40, 29.00]
+    ]
   },
 
-  "AmistadNRA": {
-    "type": "Feature",
-    "properties": { "name": "Amistad National Recreation Area" },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [
-        box(-101.30, 29.40, -100.90, 29.70)
-      ]
-    }
+  // 6. Amistad National Recreation Area
+  {
+    name: "Amistad National Recreation Area",
+    coords: [
+      [-101.20, 29.65],
+      [-101.00, 29.55],
+      [-100.95, 29.45],
+      [-101.15, 29.40],
+      [-101.25, 29.50]
+    ]
   },
 
-  "SanAntonioMissionsNHP": {
-    "type": "Feature",
-    "properties": { "name": "San Antonio Missions National Historical Park" },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [
-        box(-98.50, 29.25, -98.40, 29.40)
-      ]
-    }
+  // 7. San Antonio Missions National Historical Park
+  {
+    name: "San Antonio Missions NHP",
+    coords: [
+      [-98.50, 29.42],
+      [-98.45, 29.42],
+      [-98.45, 29.28],
+      [-98.50, 29.28]
+    ]
   },
 
-  "LBJ_NHP": {
-    "type": "Feature",
-    "properties": { "name": "Lyndon B. Johnson National Historical Park" },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [
-        box(-98.70, 30.20, -98.55, 30.33)
-      ]
-    }
+  // 8. Lyndon B. Johnson National Historical Park
+  {
+    name: "Lyndon B. Johnson NHP",
+    coords: [
+      [-98.67, 30.28],
+      [-98.62, 30.28],
+      [-98.62, 30.22],
+      [-98.67, 30.22]
+    ]
   },
 
-  "WacoMammothNM": {
-    "type": "Feature",
-    "properties": { "name": "Waco Mammoth National Monument" },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [
-        box(-97.20, 31.57, -97.15, 31.65)
-      ]
-    }
+  // 9. Waco Mammoth National Monument
+  {
+    name: "Waco Mammoth National Monument",
+    coords: [
+      [-97.19, 31.63],
+      [-97.17, 31.63],
+      [-97.17, 31.60],
+      [-97.19, 31.60]
+    ]
   },
 
-  "AlibatesFlintQuarriesNM": {
-    "type": "Feature",
-    "properties": { "name": "Alibates Flint Quarries National Monument" },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [
-        box(-101.75, 35.52, -101.62, 35.63)
-      ]
-    }
+  // 10. Alibates Flint Quarries National Monument
+  {
+    name: "Alibates Flint Quarries NM",
+    coords: [
+      [-101.70, 35.60],
+      [-101.67, 35.60],
+      [-101.67, 35.57],
+      [-101.70, 35.57]
+    ]
   },
 
-  "LakeMeredithNRA": {
-    "type": "Feature",
-    "properties": { "name": "Lake Meredith National Recreation Area" },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [
-        box(-102.00, 35.45, -101.60, 35.75)
-      ]
-    }
+  // 11. Lake Meredith National Recreation Area
+  {
+    name: "Lake Meredith NRA",
+    coords: [
+      [-101.85, 35.75],
+      [-101.60, 35.80],
+      [-101.55, 35.65],
+      [-101.70, 35.55],
+      [-101.85, 35.60]
+    ]
   },
 
-  "BigThicketNP": {
-    "type": "Feature",
-    "properties": { "name": "Big Thicket National Preserve" },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [
-        box(-94.50, 30.05, -93.90, 30.45)
-      ]
-    }
+  // 12. Big Thicket National Preserve
+  {
+    name: "Big Thicket National Preserve",
+    coords: [
+      [-94.38, 30.45],
+      [-94.10, 30.45],
+      [-94.05, 30.10],
+      [-94.30, 30.05]
+    ]
   },
 
-  "PadreIslandNS": {
-    "type": "Feature",
-    "properties": { "name": "Padre Island National Seashore" },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [
-        box(-97.55, 26.80, -97.20, 27.70)
-      ]
-    }
+  // 13. Padre Island National Seashore
+  {
+    name: "Padre Island National Seashore",
+    coords: [
+      [-97.48, 27.80],
+      [-97.25, 27.80],
+      [-97.25, 26.30],
+      [-97.48, 26.30]
+    ]
   },
 
-  "PaloAltoBattlefieldNHP": {
-    "type": "Feature",
-    "properties": { "name": "Palo Alto Battlefield National Historical Park" },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [
-        box(-97.25, 26.05, -97.09, 26.13)
-      ]
-    }
+  // 14. Palo Alto Battlefield National Historical Park
+  {
+    name: "Palo Alto Battlefield NHP",
+    coords: [
+      [-97.50, 26.10],
+      [-97.40, 26.10],
+      [-97.40, 26.00],
+      [-97.50, 26.00]
+    ]
   }
-};
+
+];
 
 
